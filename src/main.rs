54c3bcfe5deb764dfccb0ab03e05b5f39bd90748
Laura Powles <laura@laurapowles.com>
2021-09-08 18:26:07 +1000

    added command line argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{load_yaml, App, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+
+    #[allow(unused_variables)]
+    let matches = App::from_yaml(yaml)
+        .set_term_width(0)
+        .global_settings(&[
+            AppSettings::DisableHelpSubcommand,
+            AppSettings::DeriveDisplayOrder,
+            AppSettings::SubcommandRequiredElseHelp,
+        ])
+        .get_matches();
 }
